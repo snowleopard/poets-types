b9ab36d58d482a989941fabf19e81285afd6007b
Andrey Mokhov <andrey.mokhov@gmail.com>
2016-08-09 10:58:17 +0100

    Further drafring

--- src/Poets.hs
+++ src/Poets.hs
@@ -1,10 +1,12 @@
 module Poets where
 
-newtype Id = Id { fromId :: Int } deriving (Eq, Show)
+import Control.Monad.RWS.Lazy
+
+data DeviceId
 
 -- Device type is parameterised by the types of properties (p) and state (s)
 data Device p s = Device
-    { deviceId         :: Id
+    { deviceId         :: DeviceId
     , deviceProperties :: p
     , deviceState      :: s
     , deviceInputs     :: [InputPort]
@@ -18,7 +20,32 @@ data RTS
 -- * d -- device properties
 -- * s -- device state
 -- * e --
-type ReceiveHandler g d ds e es = g -> d -> ds -> e -> es -> (ds, es, RTS)
-
+-- type ReceiveHandler = Graph gp -> Device dp ds do -> Edge ep es -> (ds, es, [do])
 
 -- data Edge s p
+
+-- data Pin = Pin
+--     { deviceId :: DeviceId
+--     , edgeId   :: EdgeId }
+
+
+-- An event is a message that arrived via an edge.
+data Event e a = Event
+    { edge    :: e
+    , message :: a }
+
+data GraphProperties
+data VertexProperties
+data EdgeProperties
+
+data Context = Context
+    { graphProperties  :: GraphProperties
+    , vertexProperties :: VertexProperties
+    , edgeProperties   :: EdgeProperties }
+
+data State
+data Log
+
+type VertexComputation m a = RWST Context Log State m a
+
+type EventHandler m a = Event -> VertexComputation m a
