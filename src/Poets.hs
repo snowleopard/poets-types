e81aa71bb5591fd7e6c203f40b23ff095fdf3f24
Andrey Mokhov <andrey.mokhov@gmail.com>
2016-08-12 17:13:19 +0100

    Add example

--- src/Poets.hs
+++ src/Poets.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TypeFamilies, RankNTypes #-}
+{-# LANGUAGE TypeFamilies, RankNTypes, GADTs #-}
 
 module Poets where
 
@@ -11,56 +11,68 @@ type family State a
 data Graph a = Graph
     { graphProperties :: Properties (Graph a)
     , graphVertices   :: [Vertex a]
-    , graphEdges      :: forall t. [Edge a t] }
+    , graphEdges      :: [AbstractEdge a] }
 
 data Vertex a = Vertex
     { vertexProperties :: Properties (Vertex a)
     , vertexState      :: State      (Vertex a)
-    , inputPorts       :: forall t. [InputPort a t]
-    , outputPorts      :: forall t. [OutputPort a t] }
+    , vertexPorts      :: [AbstractPort a] }
+
+-- preset :: Vertex a -> [Edge a e]
+-- preset (Vertex _ _ ps) = catMaybes getPresetEdge ps
+
+-- getPresetEdge :: AbstractPort a -> Edge a e
+-- getPresetEdge (Input (InputPort _ e)) = Just e
+-- getPresetEdge _                       = Nothing
 
 updateVertexState :: State (Vertex a) -> Vertex a -> Vertex a
-updateVertexState s (Vertex p _ is os) = Vertex p s is os
+updateVertexState s (Vertex p _ ps) = Vertex p s ps
 
--- Edges are parameterised by phantom edge type t
-data Edge a t = Edge
-    { edgeProperties :: Properties (Edge a t)
-    , edgeState      :: State      (Edge a t) }
+-- Edges are parameterised by phantom edge type e
+data Edge a e = Edge
+    { edgeProperties :: Properties (Edge a e)
+    , edgeState      :: State      (Edge a e) }
 
--- Edges of type t accept messages of type Message t
-type family Message t
+-- Edges of type e accept messages of type Message e
+type family Message e
 
-updateEdgeState :: State (Edge a t) -> Edge a t -> Edge a t
+data family AbstractEdge a
+
+updateEdgeState :: State (Edge a e) -> Edge a e -> Edge a e
 updateEdgeState s (Edge p _) = Edge p s
 
-data InputPort a t = InputPort
+data InputPort a e = InputPort
     { inputPortVertex :: Vertex a
-    , inputPortEdge   :: Edge a t }
+    , inputPortEdge   :: Edge a e }
 
-data OutputPort a t = OutputPort
+data OutputPort a e = OutputPort
     { outputPortVertex :: Vertex a
-    , outputPortEdge   :: Edge a t }
+    , outputPortEdge   :: Edge a e }
 
-type ReceiveHandler a t m = Monad m => Properties (Graph a)
-                                    -> InputPort a t
-                                    -> Message t
+data AbstractPort a where
+    Input  :: InputPort  a e -> AbstractPort a
+    Output :: OutputPort a e -> AbstractPort a
+
+type ReceiveHandler a e m = Monad m => Properties (Graph a)
+                                    -> InputPort a e
+                                    -> Message e
                                     -> m ( State (Vertex a)
-                                         , State (Edge a t)
-                                         , [OutputPort a t] )
+                                         , State (Edge a e)
+                                         , [OutputPort a e] )
 
-trivialReceiveHandler :: ReceiveHandler a t m
+trivialReceiveHandler :: ReceiveHandler a e m
 trivialReceiveHandler _ i _ = return ( vertexState (inputPortVertex i)
                                      , edgeState   (inputPortEdge   i)
                                      , [] )
 
-type SendHandler a t m = Monad m => Properties a
-                                 -> OutputPort a t
-                                 -> m ( Maybe (Message t)
+type SendHandler a e m = Monad m => Properties a
+                                 -> OutputPort a e
+                                 -> m ( Maybe (Message e)
                                       , State (Vertex a)
-                                      , State (Edge   a t)
-                                      , [OutputPort a t] )
+                                      , State (Edge   a e)
+                                      , [OutputPort a e] )
 
-trivialSendHandler :: SendHandler a t m
+trivialSendHandler :: SendHandler a e m
 trivialSendHandler _ o = return ( Nothing
                                 , vertexState (outputPortVertex o)
                                 , edgeState   (outputPortEdge   o)
@@ -68,11 +80,77 @@ trivialSendHandler _ o = return ( Nothing
 
 
 
-data Etx = Etx { etxNetworkName :: String }
+data Etx = Etx { etxFilePath :: FilePath }
 
 data EtxProperties = EtxProperties { numVertices :: Int, numEdges :: Int }
 
-type instance Properties Etx = EtxProperties
+type instance Properties (Graph  Etx) = EtxProperties
+type instance Properties (Vertex Etx) = String
+type instance State      (Vertex Etx) = Int
+
+data instance AbstractEdge Etx where
+    BlueEdge :: Edge Etx Blue -> AbstractEdge Etx
+    RedEdge  :: Edge Etx Red  -> AbstractEdge Etx
+
+graph :: Graph Etx
+graph = Graph (EtxProperties 2 2) [a, b] [RedEdge e, BlueEdge f]
+
+a :: Vertex Etx
+a = Vertex "a" 0 []
+
+b :: Vertex Etx
+b = Vertex "b" 0 []
+
+data Red
+data Blue
+
+type instance Properties (Edge Etx Red ) = Int
+type instance Properties (Edge Etx Blue) = Bool
+type instance State      (Edge Etx Red ) = Int
+type instance State      (Edge Etx Blue) = Bool
+
+type instance Message Red  = (String, Int)
+type instance Message Blue = String
+
+poets2016 :: Message Red
+poets2016 = ("POETS", 2016)
+
+hello :: Message Blue
+hello = "hello"
+
+e :: Edge Etx Red
+e = Edge 1 1
+
+f :: Edge Etx Blue
+f = Edge False False
+
+receive :: ReceiveHandler Etx e m
+receive properties (InputPort vertex edge) message = case edge of
+    RedEdge  -> return ( vertexState vertex + 1
+                            , edgeState edge + 1
+                            , [] )
+    BlueEdge -> return ( vertexState vertex + numVertices properties
+                            , True
+                            , [] )
+
+-- type ReceiveHandler a e m = Monad m => Properties (Graph a)
+--                                     -> InputPort a e
+--                                     -> Message e
+--                                     -> m ( State (Vertex a)
+--                                          , State (Edge a e)
+--                                          , [OutputPort a e] )
+
+-- trivialReceiveHandler :: ReceiveHandler a e m
+-- trivialReceiveHandler _ i _ = return ( vertexState (inputPortVertex i)
+--                                      , edgeState   (inputPortEdge   i)
+--                                      , [] )
+
+-- type SendHandler a e m = Monad m => Properties a
+--                                  -> OutputPort a e
+--                                  -> m ( Maybe (Message e)
+--                                       , State (Vertex a)
+--                                       , State (Edge   a e)
+--                                       , [OutputPort a e] )
 
 
 -- orchestrate :: Monad m
